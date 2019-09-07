843ea3289e52546d78cccb3ca0983b405f9b6994
David Bonnecaze <davidbonnecaze@davids-mbp.lan>
2019-09-07 17:19:28 -0400

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{ 
+  public class DataContext : DbContext
+  {
+    public DbSet<Value> Values { get; set; }
+
+    public DataContext(DbContextOptions<DataContext> options)
+      : base(options)
+    {
+    }
+  }
+}
